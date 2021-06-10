fa163416c0c8e7ae4e57720ad4619da0ded4fdba
cleocleocleo <cleo.gardner@gmail.com>
2021-06-10 10:26:35 -0400

    Develop - Initial commit.
    Confirmed DB/seeds are working.

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,14 @@
 const Sequelize = require("sequelize");
+require("dotenv").config();
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
-});
+const db = new Sequelize(
+    process.env.DATABASE,
+    process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD,
+    {
+        logging: false,
+        dialect: 'postgres'
+    },
+);
 
 module.exports = db;
