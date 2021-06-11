b296a63124b3f6bc128132ae5c708ed56bb1cbca
cleocleocleo <cleo.gardner@gmail.com>
2021-06-11 13:02:47 -0400

    Enable httpOnly cookies for JWTs
    ❗️ New server dependency: CORS ❗️
    Server now uses CORS and cookie-parser middleware.
    Auth will now respond with httpOnly cookies.
    Client-side utils will no longer store token data in localStorage.

--- server/app.js
+++ server/app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const { join } = require("path");
 const logger = require("morgan");
 const jwt = require("jsonwebtoken");
+const cors = require('cors');
 const session = require("express-session");
+const cookieParser = require('cookie-parser');
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const db = require("./db");
 const { User } = require("./db/models");
@@ -13,29 +15,33 @@ const sessionStore = new SequelizeStore({ db });
 const { json, urlencoded } = express;
 
 const app = express();
-
+app.use(cors());
 app.use(logger("dev"));
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(express.static(join(__dirname, "public")));
 
+// Enable cookie parser middleware
+app.use(cookieParser());
+
 app.use(function (req, res, next) {
-  const token = req.headers["x-access-token"];
-  if (token) {
-    jwt.verify(token, process.env.SESSION_SECRET, (err, decoded) => {
-      if (err) {
+    //   const token = req.headers["x-access-token"];
+    const token = req.cookies.token
+    if (token) {
+        jwt.verify(token, process.env.SESSION_SECRET, (err, decoded) => {
+            if (err) {
+                return next();
+            }
+            User.findOne({
+                where: { id: decoded.id },
+            }).then((user) => {
+                req.user = user;
+                return next();
+            });
+        });
+    } else {
         return next();
-      }
-      User.findOne({
-        where: { id: decoded.id },
-      }).then((user) => {
-        req.user = user;
-        return next();
-      });
-    });
-  } else {
-    return next();
-  }
+    }
 });
 
 // require api routes here after I create them
@@ -44,19 +50,19 @@ app.use("/api", require("./routes/api"));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  console.log(err);
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+    // set locals, only providing error in development
+    console.log(err);
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.json({ error: err });
+    // render the error page
+    res.status(err.status || 500);
+    res.json({ error: err });
 });
 
 module.exports = { app, sessionStore };
